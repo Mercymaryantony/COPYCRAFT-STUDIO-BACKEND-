d62382db94c3013f82990536b39cde1bc3f42cf5
shaniba17 <shanibatm2002@gmail.com>
2024-06-18 11:38:38 +0530

    LogIn added

--- app.js
+++ app.js
@@ -36,6 +36,38 @@ app.post("/signup", async (req, res) => {
 
 })
 
+app.post("/login",(req,res)=>{
+    let input =req.body
+    usermodel.find({"email":req.body.email}).then(
+        (response)=>{
+            if(response.length>0)
+                {
+                    let dbPassword=response[0].password
+                    console.log(dbPassword)
+                    bcrypt.compare(input.password,dbPassword,(error,isMatch)=>{
+                        if (isMatch) {
+                            jwt.sign({email:input.email},"copycraft-app",{expiresIn:"3d"},
+                                (error,token)=>{
+                                if (error) {
+                                    res.json({"status":"unable to create token"})
+                                } else {
+                                    res.json({"status":"success","userid":response[0]._id,"token":token})
+                                }
+                            })
+                        } else {
+                            res.json({"status":"incorrect password"})
+                        }
+                    })
+                }
+            else{
+                res.json({"status":"user not found"})
+            }
+        }
+    )
+    })
+    
+
+
 app.listen(8080, () => {
 
     console.log("Server started")
